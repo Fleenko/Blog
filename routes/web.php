14bb396e4eccdee62ec4419bd37ef2bcc02716ed
unknown <b-size3@ya.ru>
2022-08-31 14:25:03 +0700

    Added admin frontend

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,8 @@ Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
     Route::get('/', 'IndexController')->name('index');
 });
 
+Route::group(['namespace' => 'App\Http\Controllers\Admin\Main', 'prefix' => 'admin'], function () {
+    Route::get('/', 'IndexController')->name('index');
+});
+
 Auth::routes();
